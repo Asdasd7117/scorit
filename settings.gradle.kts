82352fc990e6f3263b115ab6284b4c5f9a6fd7c3
Asdasd7117 <alywm100@gmail.com>
2026-01-23 02:03:06 +0300

    رفع الملف settings.gradle.kts عبر PHP

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SecureGuard SDK"
+
+// الموديولات الموجودة بالمشروع
+include(":app")
+include(":securitysdk")   // ← تأكد أن اسم الموديول SDK هو securitysdk
